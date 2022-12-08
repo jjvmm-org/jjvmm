f83ff6534a8c2e3e2ec6b97f59de29edd6115efc
bluemiaomiao <xv2017@outlook.com>
2022-12-08 17:18:15 +0800

    Update

---
+++ libcore/exception/config_key_not_exist_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+class ConfigKeyNotExistException(Exception):
+    """
+    指定 Key 在配置文件中不存在
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
