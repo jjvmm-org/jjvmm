2679e86256c75846debc60f6f827b47c87deb2ae
bluemiaomiao <xv2017@outlook.com>
2022-12-12 17:14:50 +0800

    Update

--- libcore/config/config.py
+++ libcore/config/config.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import platform, os, getpass
+import os
+import getpass
+import platform
 import configparser
 
 from libcore.util.string_util import StringUtil
 from libcore.exception.config_key_not_exist_exception import ConfigKeyNotExistException
 from libcore.exception.not_support_system_type_exception import NotSupportSystemTypeException
 from libcore.exception.get_system_info_exception import GetSystemInfoException
+from libcore.exception.config_file_parse_failed_exception import ConfigFileParseFailedException
 
 
 class Config:
@@ -15,13 +18,19 @@ class Config:
     配置
     """
 
-    __mirror = None
-    __lang = "en_US"
-    __publisher = "Oracle"
+    __default_mirror = None
+    __default_lang = "en_US"
+    __default_publisher = "Oracle"
 
-    __config_file_windows = "{system_root}\\Users\\{username}\\AppData\\Local\\jjvmm\\config\\.jjvmm-config.ini"
-    __config_file_osx = "/Users/{username}/.jjvmm/Config/.jjvmm-config.ini"
-    __config_file_linux = "/home/{username}/.jjvmm/config/.jjvmm-config.ini"
+    __config = None
+
+    __config_file_windows_tpl = "{system_root}\\Users\\{username}\\AppData\\Local\\jjvmm\\config\\.jjvmm-config.ini"
+    __config_file_osx_tpl = "/Users/{username}/.jjvmm/Config/.jjvmm-config.ini"
+    __config_file_linux_tpl = "/home/{username}/.jjvmm/config/.jjvmm-config.ini"
+
+    __config_file_windows = None
+    __config_file_osx = None
+    __config_file_linux = None
 
     __curr_system_type = None
     __curr_windows_system_root = "C:"
@@ -59,18 +68,19 @@ class Config:
         else:
             raise NotSupportSystemTypeException("Unrecognized operating system.")
 
-    def __init_config_file(self):
+    def __init_config_file_location(self):
         """
         初始化配置文件位置
         :return:
         """
         if self.__curr_system_type == "OSX":
-            self.__config_file_osx.format(username=self.__curr_username)
+            self.__config_file_osx = self.__config_file_osx_tpl.format(username=self.__curr_username)
         elif self.__curr_system_type == "Windows":
-            self.__config_file_windows.format(system_root=self.__curr_windows_system_root,
-                                              username=self.__curr_username)
+            self.__config_file_windows = self.__config_file_windows_tpl.format(
+                system_root=self.__curr_windows_system_root,
+                username=self.__curr_username)
         elif self.__curr_system_type == "Linux":
-            self.__config_file_linux.format(username=self.__curr_username)
+            self.__config_file_linux = self.__config_file_linux_tpl.format(username=self.__curr_username)
         else:
             pass
 
@@ -83,9 +93,21 @@ class Config:
         :return:
         """
 
+        # TODO 通过操作系统路径加载 .jjvmm-config.ini
+
+        filename = ""
+        if os.path.exists(filename):
+            self.__config = configparser.ConfigParser()
+            self.__config.read(filename, encoding="UTF-8")
+
+            sections = self.__config.sections()
+
+            if "app" not in sections:
+                raise ConfigFileParseFailedException(filename)
+
     def __init__(self):
         self.__init_system_info()
-        self.__init_config_file()
+        self.__init_config_file_location()
         self.__load_config_file()
 
     def get(self, key: str) -> str:
@@ -101,9 +123,21 @@ class Config:
         if key not in self.__allow_config_keys:
             raise ConfigKeyNotExistException("{} is not in config file, the specified key is illegal.".format(key))
 
-        # TODO 读取配置文件中的 Value
+        key = key.strip()
 
-        return ""
+        if self.__config is None:
+            return self.__match_config_key()
+        else:
+            val = self.__config.get("app", key).strip()
+            return self.__match_config_key(key=key) if StringUtil.is_empty(val) else val
+
+    def __match_config_key(self, key: str) -> str:
+        if key == "mirror":
+            return self.__default_mirror
+        elif key == "lang":
+            return self.__default_lang
+        elif key == "publisher":
+            return self.__default_publisher
 
     def set(self, key: str, value: str) -> bool:
         """
@@ -116,7 +150,7 @@ class Config:
 
     def get_with_default(self, key: str, default: str):
         """
-        获取配置项, 如果这个配置项的值为空，那么返回 default
+        获取配置项, 如果这个配置项的值为空，那么返回用户指定的 default
         :param key: Key
         :return: Value
         :param default: 默认值
---
+++ libcore/exception/config_file_parse_failed_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+class ConfigFileParseFailedException(Exception):
+    """
+    配置文件解析失败
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
